6c52b52e7ede27d89f2c642025ca8604b9ef8423
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-10-23 11:46:11 +0000

    display blog lists

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function getByLimit(int $limit_count = 3)
+    {
+        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
+
+    public function getPaginateByLimit(int $limit_count = 5)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PostController::class, 'index']);
 
 Route::get('/posts', [PostController::class, 'index']);
