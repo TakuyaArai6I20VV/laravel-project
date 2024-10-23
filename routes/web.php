a4218b18c31ae5c1d36ac9a0cb796af6b2472c4a
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-10-23 13:22:51 +0000

    edit blog

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::post('/posts', [PostController::class, 'store']);
 Route::get('posts/create', [PostController::class, 'create']);
 
 Route::get('/posts/{post}', [PostController::class, 'show']);
+
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class,'update']);
