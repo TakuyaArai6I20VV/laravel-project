04decc77d45651f1c7024d64c6cc6a908d454dbb
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-10-23 14:05:09 +0000

    delete blog

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,5 +43,11 @@ class PostController extends Controller
         
         return redirect('/posts/' . $post->id);
     }
+
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
 }
 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
@@ -23,4 +24,7 @@ class Post extends Model
         'title',
         'body',
     ];
+
+    use SoftDeletes;
 }
+
